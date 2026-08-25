36578e5c8627cea7e080a9cd0aabe61b17dd280d
Thabiso-Thuso <ramaemathuso@gmail.com>
2026-08-25 14:34:11 +0200

    Fix typo in baseUrl configuration

--- Documentation/docusaurus.config.ts
+++ Documentation/docusaurus.config.ts
@@ -23,7 +23,7 @@ const config: Config = {
   url: 'https://thabiso-thuso.github.io',  
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/Olympiad-Documentation/',
+  baseUrl: '/Olymipad-Documentation/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
